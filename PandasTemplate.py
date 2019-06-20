446d4a6bd0b7e2cbc3a52771d721d79a2dbed169
louisbinging <louisbinging@gmail.com>
2019-06-20 14:34:35 +0800

    1.学习功能：数据清洗，数据筛选，数据提取

--- PandasTemplate.py
+++ PandasTemplate.py
@@ -8,7 +8,7 @@ from io import StringIO
 
 # ------------------------------------------------------------------------------------------
 # 数据导入
-fileR = r'Inputs\TOP55客户统谈出账收入统计-汇总.xlsx'
+fileR = r'Inputs\Test.xlsx'
 xlsx = pd.ExcelFile(fileR)
 sidf_header = pd.read_excel(xlsx, sheet_name=1, header=[0,])
 
@@ -20,7 +20,22 @@ print(sidf_header.dtypes)
 # print(sidf_header)
 
 # ------------------------------------------------------------------------------------------
+# 数据清洗
+sidf_header.fillna(value=0,inplace=True)
+
+# ------------------------------------------------------------------------------------------
+# 数据筛选
+bz = sidf_header.loc[sidf_header['客户名称']=='B站']
+
+# ------------------------------------------------------------------------------------------
 # 数据统计
 idf1 = sidf_header.groupby('省份')['10月汇总'].agg([np.sum,np.mean])
 idf2 = sidf_header.pivot_table(index=['省份'],values=['10月汇总'],aggfunc=['sum','count'])
 print(idf1)
+
+# ------------------------------------------------------------------------------------------
+# 数据提取
+idf1.loc[['上海市','江苏','浙江'],'sum']
+idf1.sort_values('sum')
+
+
