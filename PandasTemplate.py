ccaa3612af0df1d3ba47f04caa2cbf338e62df77
louisbinging <louisbinging@gmail.com>
2019-06-25 15:10:02 +0800

    1.新老数据重复比对：新老数据导入-新老数据打标签-合并-选择需重复比对列-进行重复比对并进行重复标记 2.目的：用于比对最新提取数据是否完全包含历史提取数据

--- PandasTemplate.py
+++ PandasTemplate.py
@@ -13,6 +13,9 @@ inputsList = TxtOperator.readTxt2List(txtFile,False)
 print(inputsList)
 
 folder = inputsList[0]
+preFile = inputsList[1]
+newFile = inputsList[2]
+
 print(folder)
 # 相关输出文件设置
 outfile_xls = folder + '\\' + folder[folder.rfind('\\')+1:] + '_PANDAS汇总_' + '.xlsx'
@@ -25,41 +28,53 @@ if(isx):
 # 相关输入：目录下所有文件
 fileList = FileOperator.getAllFiles(folder,notDeeep=False)
 print(len(fileList))
+
+# # ------------------------------------------------------------------------------------------
+# # 数据导入
+# exlist = []
+# headerlist = []
+# headerrows = 3
+# oldexlist = []
+# for file in fileList:
+#     if ('.xls' in file) and ('~$' not in file):
+#         print(file)
+#         sf = pd.read_excel(file, sheet_name=0, header=None, skiprows=range(headerrows))
+#         sfH = pd.read_excel(file, sheet_name=0, header=None, nrows=headerrows)
+#         # sf = pd.read_excel(file, sheet_name=0, header=[0,1,2])
+#         # sf.columns = sidf_header.columns[0:len(sf.columns)]
+#         # sf.drop([0,1,2], inplace=True)
+#         # # 为防止合并后列的重排，将列名重置为数字
+#         # sf.columns = range(len(sf.columns))
+#         #
+#         # sf[sf == ' '] = np.nan
+#         # sf.dropna(subset=sf.columns[1:2],how='all',inplace=True)
+#         # sf[1] = sf[1].map(str.strip)
+#         #
+#         # # 新增更新时间列
+#         # t = os.path.getmtime(file)
+#         # mt = time.strftime("%Y%m%d%H%M%S", time.localtime(t))
+#         # sf['更新时间'] = mt
+#         # # exdic[file] = sf
+#         exlist.append(sf)
+#         headerlist.append(sfH)
+#
+# # 数据汇总
+# sfall = pd.concat(exlist,sort=False)
+# sf_Header = pd.concat(headerlist,sort=False)
+
 # ------------------------------------------------------------------------------------------
 # 数据导入
-exlist = []
-headerlist = []
-headerrows = 3
-oldexlist = []
-for file in fileList:
-    if '.xls' in file:
-        print(file)
-        sf = pd.read_excel(file, sheet_name=0, header=None, skiprows=range(headerrows))
-        sfH = pd.read_excel(file, sheet_name=0, header=None, nrows=headerrows)
-        # sf = pd.read_excel(file, sheet_name=0, header=[0,1,2])
-        # sf.columns = sidf_header.columns[0:len(sf.columns)]
-        # sf.drop([0,1,2], inplace=True)
-        # # 为防止合并后列的重排，将列名重置为数字
-        # sf.columns = range(len(sf.columns))
-        #
-        # sf[sf == ' '] = np.nan
-        # sf.dropna(subset=sf.columns[1:2],how='all',inplace=True)
-        # sf[1] = sf[1].map(str.strip)
-        #
-        # # 新增更新时间列
-        # t = os.path.getmtime(file)
-        # mt = time.strftime("%Y%m%d%H%M%S", time.localtime(t))
-        # sf['更新时间'] = mt
-        # # exdic[file] = sf
-        exlist.append(sf)
-        headerlist.append(sfH)
+newDf = pd.read_excel(newFile, sheet_name=0, header=[0,])
+newDf['月份'] = 'NEW'
+preDf = pd.read_excel(preFile, sheet_name=0, header=[0,])
+preDf['月份'] = 'PRE'
+sfall = pd.concat([newDf,preDf],sort=False)
+dupSet = sfall.columns[1:1+6]
+print(dupSet)
+sfall['DUP'] = sfall.duplicated(subset=dupSet,keep=False)
 
-# 数据汇总
-sfall = pd.concat(exlist,sort=False)
-sf_Header = pd.concat(headerlist,sort=False)
-
-# fileR = r'Inputs\Test.xlsx'
 # # ------------------------------------------------------------------------------------------
+# fileR = r'Inputs\Test.xlsx'
 # # 数据导入
 # xlsx = pd.ExcelFile(fileR)
 # sidf_header = pd.read_excel(xlsx, sheet_name=1, header=[0,])
@@ -97,6 +112,6 @@ sf_Header = pd.concat(headerlist,sort=False)
 # 单文件多表输出
 writer = ExcelWriter(outfile_xls,engine='xlsxwriter')
 sfall.to_excel(writer,sheet_name='数据汇总')
-sf_Header.to_excel(writer,sheet_name='表头汇总')
+# sf_Header.to_excel(writer,sheet_name='表头汇总')
 writer.save()
 
