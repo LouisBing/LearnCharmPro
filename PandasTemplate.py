71ec1353cde30a1ac855ddfb38060fec9d08bef5
louisbinging <louisbinging@gmail.com>
2019-07-02 15:20:48 +0800

    1.分场景调整程序结构，添加程序功能（清洗、导入、合并等）注释，方便后期程序复用。 2.调整原因：之前按功能划分程序，各模块之间太分散且前后关系跨度太大，不容易理解和维护。

--- PandasTemplate.py
+++ PandasTemplate.py
@@ -7,17 +7,19 @@ import TxtOperator, FileOperator
 from io import StringIO
 
 # ------------------------------------------------------------------------------------------
-# Txt输入读取
+# 输入变量-Txt
 txtFile = u'Inputs\PandasTemplate.txt'
 inputsList = TxtOperator.readTxt2List(txtFile,False)
 print(inputsList)
 
 folder = inputsList[0]
+print(folder)
 preFile = inputsList[1]
 newFile = inputsList[2]
+# ------------------------------------------------------------------------------------------
+# 场景:读取文件夹下所有Excel文件进行连接合并
 
-print(folder)
-# 相关输出文件设置
+# 根据输入文件夹生成输出文件
 outfile_xls = folder + '\\' + folder[folder.rfind('\\')+1:] + '_PANDAS汇总_' + '.xlsx'
 # 如果汇总文件已存在，直接删除
 isx = os.path.exists(outfile_xls)
@@ -25,58 +27,79 @@ if(isx):
     print('DelExistFile:', outfile_xls)
     os.remove(outfile_xls)
 
-# 相关输入：目录下所有文件
+# 获取目录下所有文件名
 fileList = FileOperator.getAllFiles(folder,notDeeep=False)
 print(len(fileList))
 
+# 数据保量相关变量
+exlist = []
+# 设置表头行数
+headerrows = 1
+headerlist = []
+oldexlist = []
+
+# 数据导入-依次读取每个Excel文件并进行连接
+for file in fileList:
+    if ('.xls' in file) and ('~$' not in file):
+        print(file)
+        sf = pd.read_excel(file, sheet_name=0, header=None, skiprows=range(headerrows))
+        sfH = pd.read_excel(file, sheet_name=0, header=None, nrows=headerrows)
+        # sf = pd.read_excel(file, sheet_name=0, header=[0,1,2])
+        # sf.columns = sidf_header.columns[0:len(sf.columns)]
+        # sf.drop([0,1,2], inplace=True)
+        # # 为防止合并后列的重排，将列名重置为数字
+        # sf.columns = range(len(sf.columns))
+        #
+        # sf[sf == ' '] = np.nan
+        # sf.dropna(subset=sf.columns[1:2],how='all',inplace=True)
+        # sf[1] = sf[1].map(str.strip)
+        #
+        # # 新增更新时间列
+        # t = os.path.getmtime(file)
+        # mt = time.strftime("%Y%m%d%H%M%S", time.localtime(t))
+        # sf['更新时间'] = mt
+        # # exdic[file] = sf
+        exlist.append(sf)
+        headerlist.append(sfH)
+
+# 数据连接
+sfall = pd.concat(exlist,sort=False)
+sf_Header = pd.concat(headerlist,sort=False)
+
+# 数据清洗
+sfall.loc[sfall[0].str.contains('注：') == True, 0] = np.nan
+sfall[sfall.isin([' ','\t','\\'])] = np.nan
+sfall.dropna(how='all',inplace=True)
 # # ------------------------------------------------------------------------------------------
-# # 数据导入
-# exlist = []
-# headerlist = []
-# headerrows = 3
-# oldexlist = []
-# for file in fileList:
-#     if ('.xls' in file) and ('~$' not in file):
-#         print(file)
-#         sf = pd.read_excel(file, sheet_name=0, header=None, skiprows=range(headerrows))
-#         sfH = pd.read_excel(file, sheet_name=0, header=None, nrows=headerrows)
-#         # sf = pd.read_excel(file, sheet_name=0, header=[0,1,2])
-#         # sf.columns = sidf_header.columns[0:len(sf.columns)]
-#         # sf.drop([0,1,2], inplace=True)
-#         # # 为防止合并后列的重排，将列名重置为数字
-#         # sf.columns = range(len(sf.columns))
-#         #
-#         # sf[sf == ' '] = np.nan
-#         # sf.dropna(subset=sf.columns[1:2],how='all',inplace=True)
-#         # sf[1] = sf[1].map(str.strip)
-#         #
-#         # # 新增更新时间列
-#         # t = os.path.getmtime(file)
-#         # mt = time.strftime("%Y%m%d%H%M%S", time.localtime(t))
-#         # sf['更新时间'] = mt
-#         # # exdic[file] = sf
-#         exlist.append(sf)
-#         headerlist.append(sfH)
+# # 场景:重复数据比对
+# # 新数据导入,新增标签列
+# newDf = pd.read_excel(newFile, sheet_name=0, header=[0,])
+# newDf[newDf=='?']=np.nan
+# newDf.dropna(subset=newDf.columns[-4:],how='all',inplace=True)
+# newDf['月份'] = 'NEW'
+# # 老数据导入,新增标签列
+# preDf = pd.read_excel(preFile, sheet_name=0, header=[0,])
+# preDf[preDf=='?']=np.nan
+# preDf.dropna(subset=preDf.columns[-3:],how='all',inplace=True)
+# dupSet = preDf.columns.drop(['签约主体','客户名称','签署地市名称'])
+# preDf['月份'] = 'PRE'
+# # 新老数据连接
+# sfall = pd.concat([newDf,preDf],sort=False)
 #
-# # 数据汇总
-# sfall = pd.concat(exlist,sort=False)
-# sf_Header = pd.concat(headerlist,sort=False)
-
-# # ------------------------------------------------------------------------------------------
-# 数据导入
-newDf = pd.read_excel(newFile, sheet_name=0, header=[0,])
-newDf[newDf=='?']=np.nan
-newDf.dropna(subset=newDf.columns[-4:],how='all',inplace=True)
-newDf['月份'] = 'NEW'
-
-preDf = pd.read_excel(preFile, sheet_name=0, header=[0,])
-preDf[preDf=='?']=np.nan
-preDf.dropna(subset=preDf.columns[-3:],how='all',inplace=True)
-dupSet = preDf.columns.drop(['签约主体','客户名称','签署地市名称'])
-preDf['月份'] = 'PRE'
-
-sfall = pd.concat([newDf,preDf],sort=False)
-
+# # 数据合并
+# sidf_name = pd.read_excel(preFile, sheet_name=3, header=[0,])
+# sidf_name.drop_duplicates(['key_desc','key_type_desc'],inplace=True)
+# sidf_name.rename(columns={'key_desc':'签约主体','key_type_desc':'客户简称'},inplace=True)
+# # sfall = pd.merge(sfall,sidf_name[['key_desc','key_type_desc']],how='left',left_on='签约主体',right_on='key_desc')
+# sfall = pd.merge(sfall,sidf_name[['签约主体','客户简称']],how='left',on='签约主体')
+#
+# # 选择要对比相关列名
+# # dupSet = sfall.columns[1:1+6]
+# # dupSet = sfall.columns.drop(['签约主体','key_desc'])
+# dupSet = dupSet.append(sfall.columns[[-1,]])
+# print(dupSet)
+# # 根据列名生成重复标签列
+# sfall['DUP'] = sfall.duplicated(subset=dupSet,keep=False)
 # ------------------------------------------------------------------------------------------
 # fileR = r'Inputs\Test.xlsx'
 # 数据导入
@@ -85,23 +108,13 @@ sfall = pd.concat([newDf,preDf],sort=False)
 # sidf_header[sidf_header =='?'] = np.nan
 # sidf_header.dropna(subset=sidf_header.columns[-3:],how='all',inplace=True)
 
-sidf_name = pd.read_excel(preFile, sheet_name=3, header=[0,])
-sidf_name.drop_duplicates(['key_desc','key_type_desc'],inplace=True)
-sfall = pd.merge(sfall,sidf_name[['key_desc','key_type_desc']],how='left',left_on='签约主体',right_on='key_desc')
-
-# # dupSet = sfall.columns[1:1+6]
-# dupSet = sfall.columns.drop(['签约主体','key_desc'])
-dupSet = dupSet.append(sfall.columns[[-1,]])
-print(dupSet)
-sfall['DUP'] = sfall.duplicated(subset=dupSet,keep=False)
-
 # # ------------------------------------------------------------------------------------------
 # # 数据检查
 # print(sidf_header.shape)
 # print(sidf_header.info())
 # print(sidf_header.dtypes)
-# # print(sidf_header)
-#
+# print(sidf_header)
+
 # # ------------------------------------------------------------------------------------------
 # # 数据清洗
 # sidf_header.fillna(value=0,inplace=True)
@@ -122,19 +135,19 @@ sfall['DUP'] = sfall.duplicated(subset=dupSet,keep=False)
 # idf1.sort_values('sum')
 
 # ------------------------------------------------------------------------------------------
-# # 单文件单表输出
+# # 场景:数据导出
+# # 根据文件名自动生成输出文件名
+# fileR = newFile
+# tNow = time.strftime("%H%M%S", time.localtime())
+# fileW = fileR[:fileR.rfind('.')]+'-PANDAS-' + tNow + '.xlsx'
+# # 单表输出
+# sfall.to_excel(fileW)
+
+# # 已知导出文件名,单文件单表输出
 # sfall.to_excel(outfile_xls, sheet_name='all')
 
-# # 单文件多表输出
-# writer = ExcelWriter(outfile_xls,engine='xlsxwriter')
-# sfall.to_excel(writer,sheet_name='数据汇总')
-# # sf_Header.to_excel(writer,sheet_name='表头汇总')
-# writer.save()
-
-
-# 数据输出
-fileR = newFile
-tNow = time.strftime("%H%M%S", time.localtime())
-fileW = fileR[:fileR.rfind('.')]+'-PANDAS-' + tNow + '.xlsx'
-# 单表输出
-sfall.to_excel(fileW)
+# 已知导出文件名,单文件多表输出
+writer = ExcelWriter(outfile_xls,engine='xlsxwriter')
+sfall.to_excel(writer,sheet_name='数据汇总')
+sf_Header.to_excel(writer,sheet_name='表头汇总')
+writer.save()
