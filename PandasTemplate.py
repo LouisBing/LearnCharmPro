739dbd042950f0dcf54ca97428a91768df6b4fd0
louisbinging <louisbing@163.com>
2019-09-30 20:56:02 +0800

    将“模板”场景代码删除（已存储在操作手册中）

--- PandasTemplate.py
+++ PandasTemplate.py
@@ -17,7 +17,7 @@ txtFile = r'Inputs\PandasTemplate.txt'
 inputsList = TxtOperator.readTxt2List(txtFile,False)
 print(inputsList)
 
-SCENE = 2
+SCENE = 1
 if SCENE == 0:  
     # ------------------------------------------------------------------------------------------
     # 场景:读取文件夹下所有Excel文件进行连接合并
@@ -222,16 +222,6 @@ elif SCENE == 2:
     sidf_header2.to_excel(writer,sheet_name='表头2')
     sidf_header3.to_excel(writer,sheet_name='表头3')
     writer.save()
-# # ------------------------------------------------------------------------------------------
-# # 数据检查
-# print(sidf_header.shape)
-# print(sidf_header.info())
-# print(sidf_header.dtypes)
-# print(sidf_header)
-
-# # ------------------------------------------------------------------------------------------
-# # 数据清洗
-# sidf_header.fillna(value=0,inplace=True)
 elif SCENE == 3:
     # ------------------------------------------------------------------------------------------
     # 场景：数据统计
@@ -260,25 +250,4 @@ elif SCENE == 3:
     group_df.to_excel(writer,sheet_name='数据分组')
     sort_df.to_excel(writer,sheet_name='数据排序')
     filtra_df.to_excel(writer,sheet_name='数据筛选')
-    writer.save()
-elif SCENE == 4:
-    # ------------------------------------------------------------------------------------------
-    # 场景:数据导出
-    # 根据输入文件名自动生成输出文件名
-    fileR = header_file
-    tNow = time.strftime("%H%M%S", time.localtime())
-    fileW = fileR[:fileR.rfind('.')]+'-PANDAS-' + tNow + '.xlsx'
-
-    # 单文件单表输出
-    sidf_header.to_excel(fileW, sheet_name='all')
-
-    单文件多表输出
-    writer = ExcelWriter(fileW,engine='xlsxwriter')
-    sf_new.to_excel(writer,sheet_name='最新数据')
-    sf_history.to_excel(writer,sheet_name='历史数据')
-    sf_Header.to_excel(writer,sheet_name='表头汇总')
-    sf_error.to_excel(writer,sheet_name='异常文件')
-    midsf.to_excel(writer,sheet_name='中间表')
-    sf_pivtable.to_excel(writer, sheet_name='透视')
-    sf_sort.to_excel(writer, sheet_name='排序')
     writer.save()
