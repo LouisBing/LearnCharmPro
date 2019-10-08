39da5e5f8c55e72a218e6d8c79b5f3928e7002f2
louisbinging <louisbinging@gmail.com>
2019-10-08 21:59:06 +0800

    代码中转：通过pip一键更新所有python库功能，后期添加多线程提到更新效率

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,13 +1,42 @@
 #-*-coding:utf-8-*-
 #%%
-print('Hello World !')
-#%%
-sum = 0
-for i in range(1,11):
-    print('this is =', i)
-    sum += i
-print('SUM=%s' % sum)
-#%%
-print(sum*sum)
+import os
+pin = 'ping www.baidu.com'
 
-#%%
+cmd = 'pip list --outdated'
+result = os.popen(cmd)
+# res = result.read()
+# print(res)
+
+lines = result.readlines()
+for paki in range(2,len(lines)):
+    pa = lines[paki]
+    pa = pa[0:pa.find(' ')]
+    # print(pa)
+    pipupc = 'pip install --upgrade %s' % pa
+    print(pipupc)
+    os.system(pipupc)
+
+# pa = lines[2]
+# pa.find(' ')
+
+# upcm = 'pip install --upgrade openpyxl'
+# result = os.popen(upcm)
+# res = result.read()
+# print(res)
+
+# result = os.popen(cmd)
+# res = result.read()
+# print(res)
+# #%%
+# print('Hello World !')
+# #%%
+# sum = 0
+# for i in range(1,11):
+#     print('this is =', i)
+#     sum += i
+# print('SUM=%s' % sum)
+# #%%
+# print(sum*sum)
+
+# #%%
