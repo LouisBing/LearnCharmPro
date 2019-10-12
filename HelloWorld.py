bd7e7bfd31f72d938e11026bbcbb238dc4fc35cc
louisbinging <louisbing@163.com>
2019-10-12 20:16:17 +0800

    代码格式化

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,9 +3,9 @@
 print('Hello World !')
 #%%
 sum = 0
-for i in range(1,11):
+for i in range(1, 11):
     print('this is =', i)
     sum += i
 print('SUM=%s' % sum)
 #%%
-print(sum*sum)
+print(sum * sum)
